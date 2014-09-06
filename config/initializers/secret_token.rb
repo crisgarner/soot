b13f2a02b98ccabc4249e1645f28a864309f33a4
Cristian Garner <ceegarner@gmail.com>
2014-09-06 14:29:05 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+DisruptHack::Application.config.secret_key_base = '5f6df1e6b34224e779f63a31b12312862fc8ffe8f23f134c3d3cd73c3d8420797b95bf797541b9fdea70991c53041db43b1d94bf7737eb0e22d89fee550605f2'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+DisruptHack::Application.config.session_store :cookie_store, key: '_disruptHack_session'
