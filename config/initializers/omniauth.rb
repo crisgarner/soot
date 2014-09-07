c5445f8a3624d4e3fb23763aec26d5f60749ce4d
Cristian Garner <ceegarner@gmail.com>
2014-09-07 13:05:51 -0700

    fixed facebook

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,5 @@
 OmniAuth.config.logger = Rails.logger
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, ENV['FACEBOOK_ID'], ENV['FACEBOOK_SECRET'], {image_size: 'large',scope: 'email,user_friends', client_options: {ssl: {ca_file: Rails.root.join('lib/assets/cacert.pem').to_s}}}
+  provider :facebook, ENV['FACEBOOK_ID'], ENV['FACEBOOK_SECRET'], {image_size: 'large',scope: 'email', client_options: {ssl: {ca_file: Rails.root.join('lib/assets/cacert.pem').to_s}}}
 end
